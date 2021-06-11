b0d066a10639298993e2a67071fe2aad813a5eeb
Ben10164 <54869170+Ben10164@users.noreply.github.com>
2021-06-11 15:53:09 -0700

    lots of work on assignment 1

--- part1/printwavbytes.cpp
+++ part1/printwavbytes.cpp
@@ -1,26 +1,74 @@
+// sites used
 // https://www.dreamincode.net/forums/topic/279619-viewing-hex-bytes-of-a-wav-file/
 // https://stackoverflow.com/questions/14733761/printf-formatting-for-hexadecimal
+// https://www.geeksforgeeks.org/convert-character-array-to-string-in-c/
+// https://stackoverflow.com/questions/10723403/char-array-to-hex-string-c
+// https://stackoverflow.com/questions/7639656/getting-a-buffer-into-a-stringstream-in-hex-representation/7639754#7639754
+
 #include <iostream>
 #include <fstream>
-#include <vector>
-#include <stdlib.h>
+#include <iomanip>
 
 using namespace std;
+
+// Actual strings or words
+void displayNextBytesStr(ifstream &is, int amount);
+string getNextBytesString(ifstream &is, int amount);
+
+// other stuff
+void displayNextBytesHex(ifstream &is, int amount);
+string getNextBytesHexString(ifstream &is, int amount);
+string convertToString(unsigned char *chunk, int amount);
 
 int main(int argc, char *argv[])
 {
    ifstream is;
    is.open("bt.wav"); // openb file
 
-   unsigned char chunk[16];
-   while (is.read(reinterpret_cast<char *>(chunk), 16)) // read the file, but it needs to be reiunterpreted as char*
+   displayNextBytesStr(is, 4); // RIFF
+   displayNextBytesHex(is, 4); // how many more bytes
+   displayNextBytesStr(is, 4);
+   displayNextBytesStr(is, 4);
+}
+
+void displayNextBytesStr(ifstream &is, int amount)
+{
+   string chunk = getNextBytesString(is, amount);
+   cout << chunk << endl;
+}
+
+string getNextBytesString(ifstream &is, int amount)
+{
+   unsigned char chunk[amount];
+   is.read(reinterpret_cast<char *>(chunk), amount);
+   string returnVal;
+   returnVal = (string) reinterpret_cast<char *>(chunk);
+   return returnVal;
+}
+
+void displayNextBytesHex(ifstream &is, int amount)
+{
+   string hexVal = getNextBytesHexString(is, amount);
+   cout << hexVal << endl; // outputs
+}
+
+string getNextBytesHexString(ifstream &is, int amount)
+{
+   unsigned char chunk[amount];                      // unsigned so there are no negative hex values, this caused me alot of trouble
+   is.read(reinterpret_cast<char *>(chunk), amount); // read the file, but it needs to be reiunterpreted as char*
+
+   string hexVal = convertToString(chunk, amount); // converts the char array of the hex literal values to a readable string of hex characters
+   return hexVal;
+}
+
+string convertToString(unsigned char *chunk, int amount)
+{
+   stringstream ss;           // new stringstream
+   ss << hex << setfill('0'); // this sets the string string to default have all 0s
+   for (int i = 0; i < amount; ++i)
    {
-      // print out the bytes
-      for (int i = 0; i < 16; i++)
-      {
-         printf("%02x", (int)chunk[i]);
-         cout << " ";
-      }
-      cout << endl;
+      ss << setw(2) << static_cast<unsigned>(chunk[i]); // setw(2) makes it so instead of 000000F9 we get F9
    }
+   string hexVal = ss.str();
+   return hexVal;
 }
